7b7007b255dd124c258ac573ca15a4edf3f352e7
Pham Xuan Phu <michealporter803@gmail.com>
2022-03-14 18:53:42 +0800

    change debug

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=True, cast=bool)
-DEBUG = os.environ.get('DEBUG')
+# DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
